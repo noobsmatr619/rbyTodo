1a376efa80e93538bd5b35960a0af91a3e2a8cf6
Abrar Tajwar <s.tajwar@students.plymouth.ac.uk>
2021-06-15 16:22:33 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :todos
   get 'home/index'
   # root 'home#index'
-  root 'todos#index'
+  # root 'todos#index'
+  root 'users#sign_in'
   get 'home/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
